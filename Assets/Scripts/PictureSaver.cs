72fadcc4b5dbb84d52250c4729eac54bcd0007bb
Roman <tolstovgamedev@gmail.com>
2019-02-03 13:41:35 +0200

    Update PictureSaver.cs

--- Assets/Scripts/PictureSaver.cs
+++ Assets/Scripts/PictureSaver.cs
@@ -7,19 +7,13 @@ public class PictureSaver : MonoBehaviour
     public void SavePicture()
     {
         Texture2D texture = FindObjectOfType<Painter>().GetTexture(); //there is only one object to draw on, so I'm free to do this
-        StartCoroutine(SaveTextureToFile(texture)); //is inside a coroutine to not to get a lag while the pic is being saved
-    }
-
-    IEnumerator SaveTextureToFile(Texture2D savedTexture)
-    {
         string fullPath = System.IO.Directory.GetCurrentDirectory();
         System.DateTime date = System.DateTime.Now;
         string fileName = "DrawingTexture.png";
         if (!System.IO.Directory.Exists(fullPath))
             System.IO.Directory.CreateDirectory(fullPath);
-        var bytes = savedTexture.EncodeToPNG();
+        var bytes = texture.EncodeToPNG();
         System.IO.File.WriteAllBytes(fullPath + fileName, bytes);
         print("Texture was successfully saved");
-        yield return null;
     }
 }
